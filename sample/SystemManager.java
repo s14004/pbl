b73c12912b10681c1e90f00d7d1683e62ced49d6
s14004 <s14004@std.it-college.ac.jp>
2015-07-21 10:03:22 +0900

    pbl

--- sample/SystemManager.java
+++ sample/SystemManager.java
@@ -70,7 +70,7 @@ public class SystemManager {
 		    "                メニュー\n" +
 		    "  従業員検索(S)\n" +
 		    "  従業員管理(JI：追加 JU：更新 JD：削除)\n" +
-		    "  顧客管理(CI：追加 CU：更新)\n" +
+		    "  顧客管理(CI：追加 CU：更新)\n" + // CI/CU
 		    "  稼働状況管理(KI：追加 KD：削除)\n" +
 		    "  終了(X)\n" +
 		    "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n",
@@ -103,7 +103,7 @@ public class SystemManager {
 		// 職種から得た従業者リストを表示し，従業員IDを入力する状態
 		sts4 = new DisplayPersonsByTypeStatus(
 		    "",
-			"p->前の3件 N->次の3件\n" + 
+		    "p->前の3件　N->次の３件\n" + 
 		    "E->検索結果一覧終了（検索条件指定に戻る）[(従業員ID),P,N,E]>",
 		    false,
 		    plist,
@@ -121,7 +121,7 @@ public class SystemManager {
 		// 氏名から得た従業者リストを表示し，従業員IDを入力する状態
 		sts7 = new DisplayPersonsByNameStatus(
 		    "",
-			"p->前の3件 N->次の3件\n" +
+		    "p->前の3件　N->次の３件\n" + 
 		    "E->検索結果一覧終了（検索条件指定に戻る）[(従業員ID),P,N,E]>",
 		    false,
 		    plist,
@@ -183,7 +183,8 @@ public class SystemManager {
 		    true
 		 );
 
-		// 従業員を追加する状態
+        // --v CI/CU
+		// 顧客を追加する状態
 		sts14 = new AddClientStatus(
 		    "",
 		    "エンターキーを押すとメニューに戻ります。>",
@@ -191,13 +192,14 @@ public class SystemManager {
 		    clist
 		 );
 
-		// 従業員の情報を更新する状態
+		// 顧客の情報を更新する状態
 		sts15 = new UpdateClientStatus(
 		    "",
 		    "更新しました。\nエンターキーを押すとメニューに戻ります。>",
 		    false,
 		    clist
 		 );
+        // --^ CI/CU
 
 		sts1.setNextStatus( "S", sts2 );
 		sts1.setNextStatus( "JI", sts8 );
